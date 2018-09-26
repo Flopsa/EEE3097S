368257c1fe50046f04b81b8ef00fe1593864a0ac
Josh Stein <josh.e.stein@gmail.com>
2018-09-26 13:34:49 +0200

    Added hardware modulator class (#10)

---
+++ hardware_modulation.py
@@ -0,0 +1,20 @@
+import pigpio
+
+class modulator(object):
+    """Hardware modulation class"""
+    def __init__(self):
+        """Connects to pi"""
+        self.pi = pigpio.pi()
+
+    def start(self, freq=38000, duty_cycle=500000):
+        """
+        Starts hardware modulation.
+        Default modulation = 38kHz, 50% duty cycle
+        """
+        self.pi.hardware_PWM(18, freq, duty_cycle)
+
+    def clear(self):
+        self.pi.wave_clear()
+
+    def stop(self):
+        self.pi.stop()
